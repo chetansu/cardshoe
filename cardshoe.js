6afed3992078b19cb0c2c2c3b9752ddac9118a35
chsuri <chsuri@cisco.com>
2018-09-03 20:34:21 +0530

    fixed issues with card_shoe getting empty

--- cardshoe.js
+++ cardshoe.js
@@ -1,7 +1,6 @@
  // CARD SHOE
 
 var cards = (function(){
-    console.log("cards");
     // SUIT ORDER WILL BE DIAMOND, CLUB, HEART, SPADE
     var suits = ["Diamond", "Clubs", "Heart", "Spade"]
     var cardsVal = ["A", "2", "3", "4", "5", "6", "7", "8", "9","10", "J","Q", "K"];
@@ -9,7 +8,6 @@ var cards = (function(){
     var cardsDealt = [];
 
     function init(){
-        console.log("init");
         var count = 0;
         for(var i =0;i<suits.length;i++)
         {
@@ -26,40 +24,34 @@ var cards = (function(){
 
     // FUNC TO RETURN THE CARDS THAT WERE ALREADY DEALT
     function getDealtCards(){
-        console.log("getDealtCards");
         return cardsDealt;
     }
 
     function shuffle(){
-        console.log("shuffle");
        init();
     }
 
     // FUNC TO DEAL CARDS
     function deal(cardToDeal = 1)
     {
-        console.log("deal");
-        console.log(cardToDeal);
-        var cardsToDeal = [];                                                        // EMPTY ARRAY
-        for(var i = 0;i<cardToDeal;i++)
-        {
-            console.log("Entering "+i)
-            var randomCardIndex = Math.floor(Math.random() * cards.length + 0);    //
-            var dealCard = cards[randomCardIndex];
-            cards.splice(randomCardIndex, 0);
-            cardsToDeal.push(dealCard);
-            cardsDealt.push(dealCard);
-
-        }
-        return cardsToDeal;
+        var chosenCards = [];                                                    // EMPTY ARRAY
+            for(var i = 0;i<cardToDeal;i++)
+            {
+                var randomCardIndex = Math.floor(Math.random() * cards.length + 0);
+                var dealCard = (cards.length > 0)? cards[randomCardIndex]: "";
+                cards.splice(randomCardIndex, 1);
+                chosenCards.push(dealCard);
+                cardsDealt.push(dealCard);
+            }
+        return chosenCards;
     }
 
+    // EXPOSED METHODS
     return {
         "shuffle":shuffle,
         "getDealtCards":getDealtCards,
         "deal":deal
     }
-})();
-
+}());
 
 exports.cards = cards;
--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 var http = require('http');
-var card_shoe = require('./card_shoe.js');
-
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    console.log("createserver");
-    res.end('Hello World! '+ card_shoe.cards.deal() +"   "+card_shoe.cards.getDealtCards());
-}).listen(8085);
+var card_shoe = require('./cardshoe.js').cards;
 
 
+// deal 1 card at a time
+for(var i =0;i<52;i++)
+{
+    console.log( card_shoe.deal(1));
+}
+
+card_shoe.shuffle();
+// deal 5 card at a time
+for(var i =0;i<11;i++)
+{
+    console.log( card_shoe.deal(5));
+}
+
+card_shoe.shuffle();
+// deal 8 cards and check the dealt cards
+for(var i = 0;i<8;i++)
+{
+    console.log(card_shoe.deal());
+}
+console.log(card_shoe.getDealtCards());
